70884d18af3914171a123f27cd40c3a0cca057c0
Adam <amatara@gmail.com>
2014-11-24 23:50:24 +1300

    fix language file to display the redirect url

--- lang/en/auth_azureoauth2.php
+++ lang/en/auth_azureoauth2.php
@@ -22,13 +22,10 @@
  * @author Adam Matara
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
- 
-
 $string['pluginname'] = 'Azure Oauth2/AD Auth';
 $string['auth_azureoauth2description'] = 'Allow a user to connect to the site using Azure AD credentials. The first time the user connects, a new account is created. <a href="'.$CFG->wwwroot.'/admin/search.php?query=authpreventaccountcreation">Prevent account creation when authenticating</a> <b>must</b> be unset.';
-$string['auth_azureclientid'] = 'Get your client ID, Secret, TenantID when you generate your Azure application. <a href="https://manage.windowsazure.com/">Windows Azure portal</a> - use the following redirect url :<br/>
-Redirect URLs: {$a->redirecturls}
-<br/>';
+$string['auth_azureclientid'] = 'Generate your clientid, secret, tenantid in using the <a href="https://manage.windowsazure.com/">Windows Azure portal</a>:
+<br/>Redirect URL: '. $CFG->httpswwwroot . '/auth/azureoauth2/azure_redirect.php';
 $string['auth_azureclientid_key'] = 'Client ID';
 $string['auth_azureclientsecret'] = 'See above.';
 $string['auth_azureclientsecret_key'] = 'Client secret';
@@ -36,10 +33,8 @@ $string['auth_azuretenantid'] = 'Hint - you can find this in the endpoints ( Azu
 $string['auth_azuretenantid_key'] = 'TenantID';
 $string['auth_appid'] = 'See Above.';
 $string['auth_appid_key'] = 'App ID';
-$string['auth_domain'] = 'Domain';
-$string['auth_domain_key'] = 'Set the domain name allowed to login.';
-
-$string['communityeditionmsg'] = '';
+$string['auth_domain'] = 'Set the domain login hint - this will redirect users to your Azure organisational login page.';
+$string['auth_domain_key'] = 'Domain';
 $string['couldnotauthenticate'] = 'The authentication failed - Please try to sign-in again.';
 $string['couldnotgetazureaccesstoken'] = 'The authentication provider sent us a communication error. Please try to sign-in again.';
 $string['couldnotauthenticateuserlogin'] = 'Authentication method error.<br/>
@@ -48,7 +43,7 @@ Please try to login again with your username and password.<br/>
 <a href="{$a->loginpage}">Try again</a>.<br/>
 <a href="{$a->forgotpass}">Forgot your password</a>?';
 $string['oauth2displaybuttons'] = 'Display buttons on login page';
-$string['oauth2displaybuttonshelp'] = 'Display the Google/Facebook/... logo buttons on the top of the login page. If you want to position the buttons yourself in your login page, you can keep this option disabled and add the following code:
+$string['oauth2displaybuttonshelp'] = 'Display the login with Azure logo on the top of the login page. If you want to position the buttons yourself in your login page, you can keep this option disabled and add the following code:
 {$a}';
 $string['auth_azuresettings'] ='Settings';
 $string['emailaddressmustbeverified'] = 'Your email address is not verified by the authentication method you selected. You likely have forgotten to click on a "verify email address" link that Google or Facebook should have sent you during your subscribtion to their service.';
